ee0a568c324294b98da2e68c87f3e718d46212f0
Jax <39040183+jaxchen100@users.noreply.github.com>
2018-08-07 13:57:41 +0800

    chinese

--- PyMySQLwrite.py
+++ PyMySQLwrite.py
@@ -7,7 +7,7 @@ Created on Fri Jun  1 13:23:29 2018
 import pymysql
 #%% MySQL连接函数
 def MySQLexecute1(inputstr):                    #替换保存
-    connection = pymysql.connect(host='localhost', port=33306, user='localuser', passwd='nodAJe3oY)GO')
+    connection = pymysql.connect(host='localhost', port=33306, user='localuser', passwd='nodAJe3oY)GO',charset='utf8')
     cursor=connection.cursor(pymysql.cursors.DictCursor)
     influencenum=cursor.execute(inputstr)       #受影响条数
     connection.commit()                         #替换保存
